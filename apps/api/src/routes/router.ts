b1f129497c79dfda010bf037d3ce6258f7f576e6
Océane Druenne <odruenne@cfai-formation.fr>
2026-06-24 12:01:33 +0900

    feat: adding cities' api

--- apps/api/src/routes/router.ts
+++ apps/api/src/routes/router.ts
@@ -1,9 +1,11 @@
 import { publicProcedure } from "../oRPC";
 import { teams } from "./teams";
 import { certifications } from "./certifications";
+import { cities } from "./cities";
 
 export const router = publicProcedure.router({
   teams,
   certifications,
+  cities,
 });
 export type Router = typeof router;
