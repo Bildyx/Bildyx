fb9f8c834dc4088478595d38f832e8ed13cd4a26
Amaury Amrani <amaury.amrani@isen-ouest.yncrea.fr>
2026-08-05 09:03:56 +0200

    fix fic devops

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -18,6 +18,7 @@ import {
   PORT,
   RPC_PREFIX,
   API_URL,
+  ALLOWED_ORIGINS,
 } from "./configuration";
 import { router } from "./routes/router";
 import { database } from "./database";
@@ -29,19 +30,6 @@ import cors from "cors";
 import { LanguageSchema } from "./models/utils/enums";
 
 const app = express();
-
-const ALLOWED_ORIGINS = [
-  "http://localhost:8000",
-  "http://localhost:3000",
-  "http://localhost:5500",
-  "http://localhost:8080",
-  "http://localhost:3001",
-  "http://localhost:5173",
-  "http://127.0.0.1:3000",
-  "http://127.0.0.1:5500",
-  "http://127.0.0.1:8000",
-  "http://127.0.0.1:8080",
-];
 
 app.use(
   cors({
